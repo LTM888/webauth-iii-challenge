e5125cefa9c0b79b5b2e887af3c2106cf85c6208
[your <leigh.tm@gmail.com>
2019-06-14 01:15:38 -0400

    hasing still needs work

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-const express = require('express');
+const server = require('./api/server');
 
-const server = express();
-
-server.use(express.json());
-
-// Import Routes
-const authRoute = require('./routes/auth');
-
-
-// Route Middleware
-server.use('/api/user', authRoute);
-
-server.listen(3000, () => console.log('Bitch I am up'));
+const port = process.env.PORT || 3000;
+server.listen(port, () => console.log(`n** Running on port ${port}`));
